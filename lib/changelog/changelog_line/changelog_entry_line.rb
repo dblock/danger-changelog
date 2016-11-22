7911f7769755a8bbe9de22e133c8e453843b03b2
Anton Domashnev <antondomashnev@gmail.com>
2016-11-22 23:39:17 +0100

    Remove commented code from changelog_line;

--- lib/changelog/changelog_line/changelog_entry_line.rb
+++ lib/changelog/changelog_line/changelog_entry_line.rb
@@ -10,9 +10,21 @@ module Danger
         return true if line =~ %r{#{CHANGELOG_ENTRY_TRAILING_REGEX}\[\#\d+\]\(https:\/\/github\.com\/.*\d+\)\: #{CHANGELOG_ENTRY_TRAILING_REGEX}}
         false
       end
-
+      
       def self.validates_as_changelog_line?(line)
         line =~ %r{#{CHANGELOG_ENTRY_TRAILING_REGEX}}
+      end
+
+      # provide an example of a CHANGELOG line based on a commit message
+      def self.example(github)
+        pr_number = github.pr_json['number']
+        pr_url = github.pr_json['html_url']
+        pr_title = github.pr_title
+                         .sub(/[?.!,;]?$/, '')
+                         .capitalize
+        pr_author = github.pr_author
+        pr_author_url = "https://github.com/#{github.pr_author}"
+        "* [##{pr_number}](#{pr_url}): #{pr_title} - [@#{pr_author}](#{pr_author_url})."
       end
     end
   end
--- lib/changelog/changelog_line/changelog_line.rb
+++ lib/changelog/changelog_line/changelog_line.rb
@@ -8,39 +8,15 @@ module Danger
         self.line = line
       end
 
+      # Match the line with the validation rules
       def valid?
         raise "ChangelogLine subclass must implement the valid? method"
       end
 
+      # Match the given line if it potentially represents the specific changelog line
       def self.validates_as_changelog_line?(line)
         abort "You need to include a function for #{self} for validates_as_changelog_line?"
       end
     end
-    # # A CHANGELOG.md file reader.
-    # module ChangelogLine
-    #   # match the PR format, with or without PR number
-    #   def self.valid?(line)
-    #     return true if line =~ %r{^\*\s[\`[:upper:]].* \- \[\@[\w\d\-\_]+\]\(https:\/\/github\.com\/.*[\w\d\-\_]+\)\.$}
-    #     return true if line =~ %r{^\*\s\[\#\d+\]\(https:\/\/github\.com\/.*\d+\)\: [\`[:upper:]].* \- \[\@[\w\d\-\_]+\]\(https:\/\/github\.com\/.*[\w\d\-\_]+\)\.$}
-    #     false
-    #   end
-    #
-    #   def self.changelog_line?(line)
-    #     return true if line =~ %r{[\`[:upper:]].* \- \[\@[\w\d\-\_]+\]\(https:\/\/github\.com\/.*[\w\d\-\_]+\)}
-    #     false
-    #   end
-    #
-    #   # provide an example of a CHANGELOG line based on a commit message
-    #   def self.example(github)
-    #     pr_number = github.pr_json['number']
-    #     pr_url = github.pr_json['html_url']
-    #     pr_title = github.pr_title
-    #                      .sub(/[?.!,;]?$/, '')
-    #                      .capitalize
-    #     pr_author = github.pr_author
-    #     pr_author_url = "https://github.com/#{github.pr_author}"
-    #     "* [##{pr_number}](#{pr_url}): #{pr_title} - [@#{pr_author}](#{pr_author_url})."
-    #   end
-    # end
   end
 end
