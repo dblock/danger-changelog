0a4b2b3f42cd49b895e9104bdf4a2c72526de25f
Andrei Subbota <subbota@gmail.com>
2025-12-16 01:10:59 +0100

    test commit

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,6 +1,7 @@
 require 'json'
 
 require 'changelog/changelog_line/changelog_entry_line'
+
 require 'changelog/changelog_line/changelog_header_line'
 require 'changelog/changelog_line/changelog_line_parser'
 require 'changelog/changelog_line/changelog_placeholder_line'
